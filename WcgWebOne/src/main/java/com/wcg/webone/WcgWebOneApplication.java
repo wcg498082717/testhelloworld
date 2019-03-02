b789af36a66bb45a5d83d15165cccca284ea8417
wcg <wcg498082717@gmail.com>
2019-03-02 16:39:03 +0800

    11111111

---
+++ WcgWebOne/src/main/java/com/wcg/webone/WcgWebOneApplication.java
@@ -0,0 +1,13 @@
+package com.wcg.webone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WcgWebOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WcgWebOneApplication.class, args);
+	}
+
+}
