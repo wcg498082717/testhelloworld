f6c82e8a043bcc167f14fa2ceebf2e85b650d057
wangchengguang <498082717@qq.com>
2019-04-02 09:57:31 +0800

    2019.4.02 日期整合

---
+++ WcgWebOne/src/main/java/com/wcg/webone/sencond/controller/HtmlController.java
@@ -0,0 +1,18 @@
+package com.wcg.webone.sencond.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+
+@Controller
+public class HtmlController {
+    @RequestMapping("/hello")
+    public String helloHtml(HashMap<String, Object> map, Model model) {
+        model.addAttribute("say","欢迎欢迎,热烈欢迎");
+        map.put("hello", "欢迎进入HTML页面");
+        return "index";
+    }
+}
