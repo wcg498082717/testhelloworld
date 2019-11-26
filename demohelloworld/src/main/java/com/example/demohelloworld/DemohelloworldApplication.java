356ca8808ff5b1fb10c1e2c60a9783d902f8af48
wangchengguang <498082717@qq.com>
2019-11-26 19:24:50 +0800

    xxxxxxxxxxx

--- demohelloworld/src/main/java/com/example/demohelloworld/DemohelloworldApplication.java
+++ demohelloworld/src/main/java/com/example/demohelloworld/DemohelloworldApplication.java
@@ -1,14 +1,14 @@
-package com.example.demo;
+package com.example.demohelloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemohelloworldApplication {
 
     public static void main(String[] args) {
-        System.out.println("aaa");
-        SpringApplication.run(DemoApplication.class, args);
+        System.out.println("aaaa");
+        SpringApplication.run(DemohelloworldApplication.class, args);
     }
 
 }
--- demohelloworld/src/test/java/com/example/demohelloworld/DemohelloworldApplicationTests.java
+++ demohelloworld/src/test/java/com/example/demohelloworld/DemohelloworldApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.demohelloworld;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class DemohelloworldApplicationTests {
 
     @Test
     void contextLoads() {
